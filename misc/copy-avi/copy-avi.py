3ead70479d4f4fb2a29b5fa264e89a3a779cd2fc
cbranum <tr1uv13q>
2013-10-01 22:02:33 -0500

    adding more codec examples

--- misc/copy-avi/copy-avi.py
+++ misc/copy-avi/copy-avi.py
@@ -8,6 +8,18 @@ size = (int(videoCapture.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
 
 # uncompressed YUV encoding
 videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('I','4','2','0'), fps, size)
+
+# MPEG-1
+# videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('P','I','M','1'), fps, size)
+
+# motion-JPEG
+#videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('M','J','P','G'), fps, size)
+
+# Ogg-Vorbis
+#videoWriter = cv2.VideoWriter('output.ogv', cv2.cv.CV_FOURCC('T','H','E','O'), fps, size)
+
+# Flash Video
+# videoWriter = cv2.VideoWriter('output.flv', cv2.cv.CV_FOURCC('F','L','V','1'), fps, size)
                               
 success, frame = videoCapture.read()
 
