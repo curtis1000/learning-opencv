89ab1e490e6d56f32d85f92667694b2377a2c711
Curtis Branum <curtis.branum@gmail.com>
2013-10-12 11:13:38 -0500

    got video encoding working with camera_capture.py on my laptop

--- misc/camera-capture/camera-capture.py
+++ misc/camera-capture/camera-capture.py
@@ -1,6 +1,6 @@
 import cv2
 
-cameraCapture = cv2.VideoCapture(0)
+cameraCapture = cv2.VideoCapture(1)
 
 if not cameraCapture:
      print "Failed to initialize capture"
@@ -12,7 +12,20 @@ int(cameraCapture.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
 
 print size
 
-videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('I','4','2','0'), fps, size)
+# works on my win 7 laptop for encoding to a playable file
+videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('i','Y', 'U', 'V'), fps, size)
+
+# MPEG-1
+#videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('P','I','M','1'), fps, size)
+
+# motion-JPEG
+#videoWriter = cv2.VideoWriter('output.avi', cv2.cv.CV_FOURCC('M','J','P','G'), fps, size)
+
+# Ogg-Vorbis
+#videoWriter = cv2.VideoWriter('output.ogv', cv2.cv.CV_FOURCC('T','H','E','O'), fps, size)
+
+# Flash Video
+#videoWriter = cv2.VideoWriter('output.flv', cv2.cv.CV_FOURCC('F','L','V','1'), fps, size)
 
 success, frame = cameraCapture.read()
 print success
